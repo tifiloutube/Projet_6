095d9d2a824f88dc47a3b7992e4511ec0138cf5a
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2021-07-19 17:19:37 +0200

    JS
    - Le frontend peut récupérer les images et les afficher lorsque l'on ajoute un objet.
    - Modification des éléments suivant possible (name, manufacturer, description, heat)
    - L'affichage des ingrédients ne se fait pas.
    - Impossible de modifier l'image après l'ajout de l'objet.

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -29,27 +29,14 @@ exports.getOneThing = (req, res, next) => {
 };
 
 exports.modifyThing = (req, res, next) => {
-    const thing = new Thing({
-        _id: req.params.id,
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-    });
-    Thing.updateOne({_id: req.params.id}, thing).then(
-        () => {
-            res.status(201).json({
-                message: 'Thing updated successfully!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
+    const thingObject = req.file ?
+        {
+            ...JSON.parse(req.body.thing),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+    Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+        .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteThing = (req, res, next) => {
--- backend/models/thing.js
+++ backend/models/thing.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const thingSchema = mongoose.Schema({
     userId: {type: String, require: true},
     name: {type: String, require: true},
+    description: {type: String, require: true},
     manufacturer: {type: String, require: true},
     mainPepper: {type: String, require: true},
-    imageURL: {type: String, require: true},
-    heat: {type: Number, require: true}
+    imageUrl: {type: String, require: true},
+    heat: {type: Number, require: true},
+    likes: {type: Number, require: true},
+    dislikes: {type: Number, require: true}
 })
 
 module.exports = mongoose.model('Thing', thingSchema);
